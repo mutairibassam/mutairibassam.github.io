284722e7e95d53e631d5f5753c2fbc7ea6a27ed4
BassamMutairi <mutairibassam@gmail.com>
2021-06-28 19:48:56 +0300

    migrate all the code to typescript

---
+++ datum/pages/soon.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/Home.module.css'
+
+export default function Soon() {
+    return (
+        <div>
+        <main className={styles.main}>
+            <h1 className={styles.title}>
+                Coming soon!
+            </h1>
+
+            <p className={styles.description}>
+                Our team is assessing the content to make sure it&apos;s flawless
+            </p>
+        </main>
+        </div>
+    )
+}
