c2ea005d066b36d1f1c4d8bd4727be1435a44b0f
BassamMutairi <mutairibassam@gmail.com>
2021-06-29 23:00:26 +0300

    UPDATE: add registration button

---
+++ datum/pages/registration.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.scss'
+
+export default function Registration() {
+    return (
+        <div>
+        <main className={styles.main}>
+            <h1 className={styles.title}>
+                Registration form
+            </h1>
+        </main>
+        </div>
+    )
+}
