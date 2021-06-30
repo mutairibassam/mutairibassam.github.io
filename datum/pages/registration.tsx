d111c8ced930694c1262db28ae153e1147b15de7
BassamMutairi <mutairibassam@gmail.com>
2021-06-30 19:50:25 +0300

    UPDATE: add back btn

--- datum/pages/registration.tsx
+++ datum/pages/registration.tsx
@@ -1,39 +1,38 @@
 import styles from "../styles/Registration.module.scss";
+import Link from "next/link";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
 export default function Registration() {
-    return (
-        <div>
-            <main>
-                <div className={styles.container}>
-                    <div className={styles.box}>
-                        <p className={styles.description}>Registration form</p>
+  return (
+    <div>
+      <main>
+        <div className={styles.container}>
+          <div className={styles.box}>
+            <Link href="/">
+              <div className={styles.back}>
+                <AiOutlineArrowLeft />
+              </div>
+            </Link>
+            <p className={styles.description}>Registration form</p>
 
-                        <form>
-                            <div className={styles.category}>
-                                <label>Full Name:</label>
-                            </div>
-                            <input
-                                placeholder="Name"
-                                className={styles.field_input}
-                            ></input>
+            <form>
+              <div className={styles.category}>
+                <label>Full Name:</label>
+              </div>
+              <input placeholder="Name" className={styles.field_input}></input>
 
-                            <div className={styles.category}>
-                                <label>Email:</label>
-                            </div>
+              <div className={styles.category}>
+                <label>Email:</label>
+              </div>
 
-                            <input
-                                placeholder="Email"
-                                className={styles.field_input}
-                            ></input>
-                            <div>
-                                <button className={styles.submit}>
-                                    Submit
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </main>
+              <input placeholder="Email" className={styles.field_input}></input>
+              <div>
+                <button className={styles.submit}>Submit</button>
+              </div>
+            </form>
+          </div>
         </div>
-    );
+      </main>
+    </div>
+  );
 }
