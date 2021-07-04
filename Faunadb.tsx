1f67009b10449e281bdcac90535c43c76fc56aee
BassamMutairi <mutairibassam@gmail.com>
2021-07-04 20:20:38 +0300

    migrate faunadb to typescript

---
+++ Faunadb.tsx
@@ -0,0 +1,34 @@
+require("dotenv").config();
+// require('dotenv').config({path: path.resolve(__dirname+'/.env')});
+
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const faunadb = require("faunadb");
+const client = new faunadb.Client({ secret: process.env.USERSDB_API_KEY });
+
+const { Get, Match, Index, Create, Paginate, Collection, Lambda, Var, Join } =
+  faunadb.query;
+
+const q = faunadb.query;
+const addUsers = async (name, email, program) => {
+  return await client.query(
+    q.Create(q.Collection("users"), {
+      data: { name, email, program },
+    })
+  );
+};
+
+// Test api
+app.get("/test", async (req, res) => {
+  console.log("hala");
+});
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
+
+module.exports = {
+  addUsers,
+};
