79a09138358f3565fce122af7bf87b348d6b6ece
BassamMutairi <mutairibassam@gmail.com>
2021-07-25 18:29:08 +0300

    fix: import dotenv in next.config.js

--- functions/helpers/formattedReturn.js
+++ functions/helpers/formattedReturn.js
@@ -1,6 +1,6 @@
-module.exports = (statusCode, body) => {
-  return {
-    statusCode,
-    body: JSON.stringify(body),
-  };
-};
+// module.exports = (statusCode, body) => {
+//   return {
+//     statusCode,
+//     body: JSON.stringify(body),
+//   };
+// };
