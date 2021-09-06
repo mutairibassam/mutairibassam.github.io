d359975f9f39181a447b2326d99c567e8facbc12
BassamMutairi <mutairibassam@gmail.com>
2021-09-06 09:37:19 +0300

    Update integrate with google analytics

--- datum/pages/_app.tsx
+++ datum/pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import ReactGA from "react-ga";
+
+function initAnalytics() {
+  ReactGA.initialize("UA-206685696-1");
+  ReactGA.pageview("Home");
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  initAnalytics();
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
