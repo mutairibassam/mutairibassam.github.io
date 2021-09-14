8f7a09b841a6812cfe15e1a5e32ecf89065fc344
BassamMutairi <mutairibassam@gmail.com>
2021-09-14 16:40:27 +0300

    Fix add logs

--- datum/Faunadb.js
+++ datum/Faunadb.js
@@ -4,11 +4,14 @@ const port = 5000;
 
 const faunadb = require("faunadb");
 const client = new faunadb.Client({ secret: process.env.USERSDB_API_KEY });
+console.log("key", process.env.USERSDB_API_KEY)
+
 
 const { Get, Match, Index, Create, Paginate, Collection, Lambda, Var, Join } =
   faunadb.query;
 
 const q = faunadb.query;
+
 const addUsers = async (name, email, program) => {
   return await client.query(
     q.Create(q.Collection("users"), {
